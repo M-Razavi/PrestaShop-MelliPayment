ef76f679db6194d2e9e61bb1f0540baa3b73a622
Razavi <m.razavi777@gmail.com>
2011-02-14 10:30:17 +0330

    bug fixing and improvement

---
+++ invoice_melli.tpl
@@ -0,0 +1,20 @@
+</br>
+</br>
+</br>
+<fieldset id="melliFieldSet" style="width: 400px;">
+
+	<legend>
+
+     {l s='Melli Online Payment Details' mod='mellipayment'}
+
+	</legend>
+
+	<div id="melliInfo" border: solid red 1px;">
+	<table>
+	<tr><td>{l s='Payment Time:' mod='mellipayment'}</td> <td>{$timeStart}</td></tr>
+	<tr><td>{l s='Transaction ID:' mod='mellipayment'}</td> <td>{$transID}</td></tr>
+	<tr><td>{l s='Payment Amount:' mod='mellipayment'}</td> <td>{$paymentAmount}</td></tr>
+	</table>
+	</div>
+
+</fieldset>
--- receipt.php
+++ receipt.php
@@ -1,38 +1,35 @@
 <?php
-/*******************************************************************************
- *                                                                             *
- * @version  MelliPayment.php version 1.0                                      *
- * @copyright Copyright (c) 2011.                                              *
- * @license http://www.opensource.org/licenses/gpl-2.0.php GNU Public License. *
- * @author Mahdi Razavi  Razavi.Dev@gmail.com                                  *
- *                                                                             *
- *******************************************************************************/
- 
-include(dirname(__FILE__).'/../../config/config.inc.php');
-include(dirname(__FILE__).'/../../header.php');
-include(dirname(__FILE__).'/mellipayment.php');
+	/*******************************************************************************
+	*                                                                             *
+	* @version  MelliPayment.php version 1.0                                      *
+	* @copyright Copyright (c) 2011.                                              *
+	* @license http://www.opensource.org/licenses/gpl-2.0.php GNU Public License. *
+	* @author Mahdi Razavi  Razavi.Dev@gmail.com                                  *
+	*                                                                             *
+	*******************************************************************************/
 
-if (!$cookie->isLogged())
-	Tools::redirect('authentication.php?back=order.php');
-	
-$mellipayment = new mellipayment();
-$result = $mellipayment->checkReceipt($_POST,false);
+	include(dirname(__FILE__).'/../../config/config.inc.php');
+	include(dirname(__FILE__).'/../../header.php');
+	include(dirname(__FILE__).'/mellipayment.php');
 
-if($result)
-{
-	$cardholderName     = $_POST['cardholderName'];
-	$cardNumber         = $_POST['cardNumber'];
-	
-	$currency = new Currency(intval(isset($_POST['currency_payement']) ? $_POST['currency_payement'] : $cookie->id_currency));
-	$total = floatval(number_format($cart->getOrderTotal(true, 3), 2, '.', ''));
-	
-	//function validateOrder($id_cart, $id_order_state, $amountPaid, $paymentMethod = 'Unknown', $message = NULL, $extraVars = array(), $currency_special = NULL, $dont_touch_amount = false)
-	
-	$mellipayment->validateOrder($cart->id,  _PS_OS_PREPARATION_, $total, $mellipayment->displayName, NULL, NULL, $currency->id);
-	//$order = new Order($samanpayment->currentOrder);
-	//$samanpayment->writePaymentcarddetails($order->id, $cardholderName, $cardNumber);
-	
-	Tools::redirectLink(__PS_BASE_URI__.'order-confirmation.php?id_cart='.$cart->id.'&id_module='.$mellipayment->id.'&id_order='.$mellipayment->currentOrder.'&key='.$order->secure_key);
-}
-include_once(dirname(__FILE__).'/../../footer.php');
+	if (!$cookie->isLogged())
+		Tools::redirect('authentication.php?back=order.php');
+
+	$mellipayment = new mellipayment();
+	$result = $mellipayment->checkReceipt($_POST,false);
+
+	if($result)
+	{		
+		$currency = new Currency(intval(isset($_POST['currency_payement']) ? $_POST['currency_payement'] : $cookie->id_currency));
+		$total = floatval(number_format($cart->getOrderTotal(true, 3), 2, '.', ''));
+
+		$validateResult = $mellipayment->validateOrder($cart->id,  _PS_OS_PREPARATION_, $total, $mellipayment->displayName, NULL, NULL, $currency->id);
+		if($validateResult)
+		{
+			$mellipayment->saveOrderID($_POST['x_fp_sequence'],$mellipayment->currentOrder);
+		}
+
+		Tools::redirectLink(__PS_BASE_URI__.'order-confirmation.php?id_cart='.$cart->id.'&id_module='.$mellipayment->id.'&id_order='.$mellipayment->currentOrder.'&key='.$order->secure_key);
+	}
+	include_once(dirname(__FILE__).'/../../footer.php');
 ?>
